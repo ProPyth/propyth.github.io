9ddafb40b87f2144d180d8ab7cc71bceca200ac7
ProPyth <38011155+ProPyth@users.noreply.github.com>
2018-09-22 10:06:00 +0200

    Update infor

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -253,8 +253,39 @@ var BrowserDetect = {
 };
 $(document).ready(function() {
   $('#mailbutton').click(function(event) {
-    window.location = "mailto:wcanbutsel@cdenv.be";
+    window.location = "mailto:WVanButsel@cdenv.be";
   });
 });
+
+
+/*
+ * Youtube video auto-resizer script
+ * Created by Skipser.com
+*/
+ 
+$(document).ready(function() {
+  if(typeof YOUTUBE_VIDEO_MARGIN == 'undefined') {
+    YOUTUBE_VIDEO_MARGIN=5;
+  }
+  $('iframe').each(function(index,item) {
+    if($(item).attr('src').match(/(https?:)?\/\/www\.youtube\.com/)) {
+      var w=$(item).attr('width');
+      var h=$(item).attr('height');
+      var ar = h/w*100;
+      ar=ar.toFixed(2);
+      //Style iframe    
+      $(item).css('position','absolute');
+      $(item).css('top','0');
+      $(item).css('left','0');    
+      $(item).css('width','100%');
+      $(item).css('height','100%');
+      $(item).css('max-width',w+'px');
+      $(item).css('max-height', h+'px');        
+      $(item).wrap('<div style="max-width:'+w+'px;margin:0 auto; padding:'+YOUTUBE_VIDEO_MARGIN+'px;" />');
+      $(item).wrap('<div style="position: relative;padding-bottom: '+ar+'%; height: 0; overflow: hidden;" />');
+    }
+  });
+});
+
 
 var better_browser = '<div class="container"><div class="better-browser row"><div class="col-md-2"></div><div class="col-md-8"><h3>We are sorry but it looks like your Browser doesn\'t support our website Features. In order to get the full experience please download a new version of your favourite browser.</h3></div><div class="col-md-2"></div><br><div class="col-md-4"><a href="https://www.mozilla.org/ro/firefox/new/" class="btn btn-warning">Mozilla</a><br></div><div class="col-md-4"><a href="https://www.google.com/chrome/browser/desktop/index.html" class="btn ">Chrome</a><br></div><div class="col-md-4"><a href="http://windows.microsoft.com/en-us/internet-explorer/ie-11-worldwide-languages" class="btn">Internet Explorer</a><br></div><br><br><h4>Thank you!</h4></div></div>';
